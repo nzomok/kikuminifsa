05d1fc2077d6ea64b119d280cc591429490893ff
God Of Algorithm <69268936+nzomok@users.noreply.github.com>
2023-09-18 22:02:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function openMenu() {
+    document.getElementById("sidebar").style.display = "block";
+}
+
+
+function closeMenu() {
+        document.getElementById("sidebar").style.display = "none";
+    }
+
+
+
+    var loan = document.getElementByClassName("more-services");
+    var i;
+
+for (i = 0; i < loan.length; i++) {
+    loan[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var loans = this.nextElementSibling;
+      if (loans.style.maxHeight){
+        loans.style.maxHeight = null;
+      } else {
+        loans.style.maxHeight = loans.scrollHeight + "px";
+      } 
+    });
+  }
+
+
+document.addEventListener('DOMContentLoaded', function () {window.setTimeout(document.querySelector('svg').classList.add('animated'),1000);})
