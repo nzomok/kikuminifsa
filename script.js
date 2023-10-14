4f84b4a1f6bb2ca13adb1c9582bca77dc90b93f0
God Of Algorithm <69268936+nzomok@users.noreply.github.com>
2023-10-14 18:28:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ function computeloan(){
 var myvar;
 
 function loading(){
-  myvar = setTimeout(loadpage, 5000);
+  myvar = setTimeout(loadpage, 3000);
 }
 function loadpage(){
   document.getElementById("loader").style.display = "none";
@@ -217,3 +217,24 @@ function gohome(){
   window.open("index.html", "_self");
 }
 
+function directions(){
+  document.getElementById("iframe").src = "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d319.1284762501036!2d37.70609768495678!3d-1.7869972373156109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMcKwNDcnMTIuNSJTIDM3wrA0MicyMi40IkU!5e0!3m2!1sen!2ske!4v1696857654744!5m2!1sen!2ske";
+  document.getElementById('iframe').stylr.display = "block";
+
+}
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
